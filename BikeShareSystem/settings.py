03206726830b9b05e22828b704d9046ef1957313
CrossAB <56017512+CrossAB@users.noreply.github.com>
2023-12-11 15:09:29 +0000

    project

--- BikeShareSystem/settings.py
+++ BikeShareSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1gz#i=+o=55o)--jzj-m6tzv755$8m^i!$(p^d)ib=#^m^9lf9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['x22190619-devsecapp-env.eba-bnrxe24m.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
